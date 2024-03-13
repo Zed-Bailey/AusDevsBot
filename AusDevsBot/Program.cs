874307d2bfa210d591398f3e46e50c0176e07648
Zoran <emailzed03@gmail.com>
2024-03-13 15:40:27 +1100

    connecting to discord

--- AusDevsBot/Program.cs
+++ AusDevsBot/Program.cs
@@ -1,3 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-Console.WriteLine("Hello, World!");
+using AusDevsBot;
+using Discord;
+using Discord.WebSocket;
+
+// todo fix this to use microsoft.extensions.configuration
+var fileLines = await File.ReadLinesAsync(Path.Join(Directory.GetCurrentDirectory(), ".env"))
+    .ToListAsync();
+
+if (!fileLines.Any())
+{
+    Console.WriteLine($"[Error] .env file was not found! looked at: {Path.Join(Directory.GetCurrentDirectory(), ".env")}");
+    return;
+}
+
+var tokens = fileLines.Select(x =>
+{
+    var s = x.Split('=', 1);
+    return (s[0], s[1]);
+});
+
+
+
+var discordToken = tokens.First(x => x.Item1 == "TOKEN").Item2;
+ulong guildId = ulong.Parse(tokens.First(x => x.Item1 == "GUILD_ID").Item2);;
+
+
+
+DiscordSocketClient client = new DiscordSocketClient();
+
+var builder = new BotCommandBuilder();
+
+// todo use serilog
+client.Log += message =>
+{
+    Console.WriteLine($"[{message.Severity}] {message.Message}");
+    return Task.CompletedTask;
+};
+
+client.Ready += async () =>
+{
+    var guild = client.GetGuild(guildId);
+    builder = await builder
+        .WithGuild(guild)
+        .AddCommand("test-command", "A test command", async command =>
+        {
+            await command.RespondAsync("Command builder responding");
+        });
+
+    client.SlashCommandExecuted += builder.HandleIncomingSlashCommands;
+};
+
+
+await client.LoginAsync(TokenType.Bot, discordToken);
+await client.StartAsync();
+
+
+
+// infinite delay
+await Task.Delay(-1);
